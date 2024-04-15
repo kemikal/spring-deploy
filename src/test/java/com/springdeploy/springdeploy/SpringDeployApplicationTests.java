93787c7006463fb187d61e6f32c00f68b6134d70
kemikal <janne@kemikal.org>
2024-04-15 13:31:03 +0200

    test for deploy

---
+++ src/test/java/com/springdeploy/springdeploy/SpringDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdeploy.springdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
